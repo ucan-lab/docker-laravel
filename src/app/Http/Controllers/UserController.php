fe80fcb6348b19fae9fc1776d00273d53e421d95
uepricky <67101863+uepricky@users.noreply.github.com>
2024-02-12 14:07:47 +0900

    修正

--- src/app/Http/Controllers/UserController.php
+++ src/app/Http/Controllers/UserController.php
@@ -10,23 +10,22 @@ class UserController extends Controller
     public function get(Request $request)
     {
         // if文はサンプルで実装しただけで本来不要
-        if ($request->user()) {
-            $userModel = new User();
-            // $user = $userModel->find($request->user()->id);
-            $user = $userModel->find(2);
+        // if ($request->user()) {
+        //     $userModel = new User();
+        //     $user = $userModel->find($request->user()->id);
 
-            if ($user) {
-                return response()->json([
-                    'message' => 'User found successfully.',
-                    'data' => $user
-                ], 200); // HTTP ステータスコード 200
-            }
+        //     if ($user) {
+        //         return response()->json([
+        //             'message' => 'User found successfully.',
+        //             'data' => $user
+        //         ], 200); // HTTP ステータスコード 200
+        //     }
 
-            // ユーザーが見つからなかった場合
-            return response()->json([
-                'message' => 'User not found.'
-            ], 404); // HTTP ステータスコード 404
-        }
+        //     // ユーザーが見つからなかった場合
+        //     return response()->json([
+        //         'message' => 'User not found.'
+        //     ], 404); // HTTP ステータスコード 404
+        // }
 
         return $request->user();
     }

dd1aee5b1592ebaac138ad4a217255db4a04c009
uepricky <67101863+uepricky@users.noreply.github.com>
2024-02-12 13:59:43 +0900

    ユーザー認証

---
+++ src/app/Http/Controllers/UserController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    public function get(Request $request)
+    {
+        // if文はサンプルで実装しただけで本来不要
+        if ($request->user()) {
+            $userModel = new User();
+            // $user = $userModel->find($request->user()->id);
+            $user = $userModel->find(2);
+
+            if ($user) {
+                return response()->json([
+                    'message' => 'User found successfully.',
+                    'data' => $user
+                ], 200); // HTTP ステータスコード 200
+            }
+
+            // ユーザーが見つからなかった場合
+            return response()->json([
+                'message' => 'User not found.'
+            ], 404); // HTTP ステータスコード 404
+        }
+
+        return $request->user();
+    }
+}
--- src/routes/api.php
+++ src/routes/api.php
@@ -13,7 +13,14 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+use App\Http\Controllers\{
+    UserController
+};
 
-Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
-    return $request->user();
+// Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+Route::middleware(['auth:sanctum'])->group(function () {
+    Route::get('/user', [UserController::class, 'get'])->name('user.get');
 });
